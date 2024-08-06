efb6c6e71a11e89090d3232e397f365efeacc34c
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-07 00:08:24 +0530

    Context API

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,7 +1,16 @@
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./Context/UserContextProvider";
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <UserContextProvider>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
